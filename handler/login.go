de302a1da534e4f41df87591f4c6e2ebc900a4a3
willian <18702515157@163.com>
2017-08-25 18:29:17 +0800

    asdfas

--- handler/login.go
+++ handler/login.go
@@ -41,7 +41,7 @@ func (param *Login) Serve(ctx *faygo.Context) error {
 	} else {
 		token := jwt.New(jwt.SigningMethodHS256)
 		claims := make(jwt.MapClaims)
-		claims["exp"] = time.Now().Add(time.Minute * time.Duration(1)).Unix()
+		claims["exp"] = time.Now().Add(time.Hour * time.Duration(1)).Unix()
 		claims["iat"] = time.Now().Unix()
 		token.Claims = claims
 		tokenString, err := token.SignedString([]byte(constants.Secret))
