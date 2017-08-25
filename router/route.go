80eb6c98c27fffef6c47a68a59cc42fb36168765
willian <18702515157@163.com>
2017-08-25 18:20:18 +0800

    阿斯顿发

--- router/route.go
+++ router/route.go
@@ -11,42 +11,42 @@ func Route(frame *faygo.Framework) {
 	frame.Route(
 		frame.NewGroup("/frank",
 			//frame.NewOPTIONS("/DetailList.go"),
-			frame.NewPOST("/DetailList.go", &handler.FrankDetail).Use(middleware.RedisCache),
+			frame.NewPOST("/DetailList.go", &handler.FrankDetail).Use(middleware.RedisCache).Use(middleware.Auth),
 
 			//frame.NewOPTIONS("/search.go"),
 			frame.NewPOST("/search.go", &handler.Search{}).Use(middleware.RedisCache).Use(middleware.Auth),
 
 			//frame.NewOPTIONS("/topTen.go"),
-			frame.NewPOST("/topTen.go", &handler.TopTenProduct{}).Use(middleware.RedisCache),
+			frame.NewPOST("/topTen.go", &handler.TopTenProduct{}).Use(middleware.RedisCache).Use(middleware.Auth),
 
 			//frame.NewOPTIONS("/CompanyRelations.go"),
-			frame.NewPOST("/CompanyRelations.go", &handler.CompanyRelations{}).Use(middleware.RedisCache),
+			frame.NewPOST("/CompanyRelations.go", &handler.CompanyRelations{}).Use(middleware.RedisCache).Use(middleware.Auth),
 
 			//frame.NewOPTIONS("/GroupHistory.go"),
-			frame.NewPOST("/GroupHistory.go", &handler.GroupHistory{}).Use(middleware.RedisCache),
+			frame.NewPOST("/GroupHistory.go", &handler.GroupHistory{}).Use(middleware.RedisCache).Use(middleware.Auth),
 
 			//frame.NewOPTIONS("/NewTenFrank.go"),
-			frame.NewPOST("/NewTenFrank.go", &handler.NewTenFrank{}).Use(middleware.RedisCache),
+			frame.NewPOST("/NewTenFrank.go", &handler.NewTenFrank{}).Use(middleware.RedisCache).Use(middleware.Auth),
 
 			//frame.NewOPTIONS("/ProductList.go"),
-			frame.NewPOST("/ProductList.go", &handler.ProductList).Use(middleware.RedisCache),
+			frame.NewPOST("/ProductList.go", &handler.ProductList).Use(middleware.RedisCache).Use(middleware.Auth),
 
 			//frame.NewOPTIONS("/InfoDetail.go"),
-			frame.NewPOST("/InfoDetail.go", &handler.InfoDetail).Use(middleware.RedisCache),
+			frame.NewPOST("/InfoDetail.go", &handler.InfoDetail).Use(middleware.RedisCache).Use(middleware.Auth),
 
 			//frame.NewOPTIONS("/DetailOne.go"),
-			frame.NewGET("/DetailOne.go", &handler.DetailOne).Use(middleware.RedisCache),
+			frame.NewGET("/DetailOne.go", &handler.DetailOne).Use(middleware.RedisCache).Use(middleware.Auth),
 
 			//frame.NewOPTIONS("/DetailTrend.go"),
-			frame.NewPOST("/DetailTrend.go", &handler.DetailTrend{}).Use(middleware.RedisCache),
+			frame.NewPOST("/DetailTrend.go", &handler.DetailTrend{}).Use(middleware.RedisCache).Use(middleware.Auth),
 
-			frame.NewPOST("/CompanyInfo.html", &handler.CompanyInfo{}),
+			frame.NewPOST("/CompanyInfo.html", &handler.CompanyInfo{}).Use(middleware.Auth),
 
-			frame.NewPOST("/CompanyList.html", &handler.CompanyList{}),
+			frame.NewPOST("/CompanyList.html", &handler.CompanyList{}).Use(middleware.Auth),
 
-			frame.NewPOST("/CompanyDistrict.html", &handler.CompanyDistrict{}),
+			frame.NewPOST("/CompanyDistrict.html", &handler.CompanyDistrict{}).Use(middleware.Auth),
 
-			frame.NewPOST("/CompanyContacts.html", &handler.CompanyContacts{}),
+			frame.NewPOST("/CompanyContacts.html", &handler.CompanyContacts{}).Use(middleware.Auth),
 		),
 
 		frame.NewGroup("/index",
